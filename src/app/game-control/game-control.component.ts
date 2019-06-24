4eeac2c16124633e0ab881b28cefd5975e49685b
storres8 <stevetorres7562@gmail.com>
2019-06-24 15:37:35 -0400

    update.

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
   styleUrls: ["./game-control.component.css"]
 })
 export class GameControlComponent implements OnInit {
+  // need the Output decorator to give access to parent component.
+  // need the new EventEmitter to create a new event.
+  // both of which must be imported from @angular/core.
   @Output() intervalFired = new EventEmitter<number>();
   interval;
   lastNumber = 1;
@@ -16,8 +19,13 @@ export class GameControlComponent implements OnInit {
 
   handleStart() {
     this.interval = setInterval(() => {
+      // what is inside the emit() function is what is being passed and can be accessed with $event.
       this.intervalFired.emit(this.lastNumber);
       this.lastNumber++;
     }, 1000);
+  }
+
+  handlePause() {
+    clearInterval(this.interval);
   }
 }
