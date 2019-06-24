d06a6fbf8c486e2e963d091708dcb2db75e4194e
storres8 <stevetorres7562@gmail.com>
2019-06-24 15:05:39 -0400

    created click event to emit.

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-game-control',
-  templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  selector: "app-game-control",
+  templateUrl: "./game-control.component.html",
+  styleUrls: ["./game-control.component.css"]
 })
 export class GameControlComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  handleStart() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber);
+      this.lastNumber++;
+    }, 1000);
   }
-
 }
